afad6352e294919cd5738865db68e1c225a6f504
Yoepee <kdy2975@naver.com>
2025-08-01 23:27:18 +0900

    17번 - 그린

--- src/main/java/com/example/Calc.java
+++ src/main/java/com/example/Calc.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,27 +9,65 @@ public class Calc {
 
     public static int run(String expr) {
         getValue(expr);
-        int result = 0;
-        String type = "+";
 
-        for(String item : list){
-            String value = null;
-            switch (item) {
-                case "+", "-", "*" -> type = item;
-                default -> value = item;
-            }
-
-            if (type != null && value != null){
-                result = calc(type, result, Integer.parseInt(value));
-            }
+        while(list.size() > 1){
+            list = getNextList(list);
         }
-        return result;
+
+        return list.stream().mapToInt(Integer::parseInt).sum();
     }
 
     private static void getValue(String expr) {
         list = Arrays.stream(expr.split(" "))
                 .map(e -> e.trim())
                 .toList();
+    }
+
+    private static List<String> getNextList(List<String> list) {
+        List<String> newList = new ArrayList<>();
+        newList.add("0");
+        String type = "+";
+        String prevValue = "0";
+        String prevType = "+";
+        String prevResult = "0";
+        for(String item : list){
+            String value = null;
+            switch (item) {
+                case "+", "-", "*":
+                    type = item;
+                    break;
+                default:
+                    value = item;
+                    break;
+            }
+
+            if (value != null){
+                if (!type.equals("*")){
+                    int listSize = newList.size();
+                    int left = newList.get(listSize - 1).equals("0") ? 0 : Integer.parseInt(newList.get(listSize - 1));
+                    int right = value.equals("0") ? 0 : Integer.parseInt(value);
+                    newList.remove(listSize - 1);
+                    prevResult = String.valueOf(left);
+                    String result = String.valueOf(calc(type, left, right));
+                    newList.add(String.valueOf(calc(type, left, right)));
+                } else {
+                    int left = prevValue.equals("0") ? 0 : Integer.parseInt(prevValue);
+                    int right = value.equals("0") ? 0 : Integer.parseInt(value);
+                    String result = String.valueOf(calc(type, left, right));
+                    newList.add(result);
+                }
+            } else if (type.equals("*") && !prevType.equals("*")){
+                int listSize = newList.size();
+                newList.remove(listSize - 1);
+                newList.add(prevResult);
+                newList.add(prevType);
+            }
+
+            if (value != null) prevValue = value;
+            if(type != null) prevType = type;
+        }
+
+        return newList;
     }
 
     private static int calc(String type, int a, int b) {
